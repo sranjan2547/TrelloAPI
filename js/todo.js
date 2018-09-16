e04fdc29cab57fdbd0356b443c32219927e88587
sameer ranjan <sameer.ranjan@mountblue.io>
2018-09-16 17:57:51 +0530

    all functionality working for all cards

--- js/todo.js
+++ js/todo.js
@@ -1,33 +1,69 @@
 const key = '2741f097a35c836f5878ad3536c6bcf3';
 const token = '676e08999f4ddfb4bf19968bfe184927a508a6308b7f9fc84313e85fbf7e019e';
 const auth = "key=" + key + "&token=" + token;
-const boardID = "5b9a4115af329d858b2fbce3";
-var checklistId = "5b9a4a4bbef75a2b84fd4211";
-var cardId="5b9a48ed4ed5294acd430060";
-var data = null;
+var boardID;
+var checklistId;
+var cardId;
+//var data = null;
 var checklists;
 
+
+
 $(document).ready(function () {
-loadCard();
-    loadTask();
-    $("select#selectCard").change(function(){
-        checklistId = $("#selectCard option:selected").val();
-      //  alert("You have selected the country - " + selectedCountry);
-    });
+    fetch(
+            'https://api.trello.com/1/members/me/boards?key=' + key + '&token=' + token
+        ).then(boards => boards.json())
+        .then((boards) => {
+            boardID = boards[3].id;
+
+            fetch("https://api.trello.com/1/boards/" + boardID + "/checklists/?" + auth)
+                .then(response => response.json())
+                .then((response) => {
+                    //  console.log(response[0])
+                    checklistId = response[0].id;
+                    cardId = response[0].idCard;
+                })
+
+            loadCard();
+            loadTask();
+        })
+
+
+
 
 });
-function loadCard(){
+
+$("select#selectCard").change(function () {
+    checklistId = $("#selectCard option:selected").val();
+
     fetch("https://api.trello.com/1/boards/" + boardID + "/checklists/?" + auth)
-    .then(response => response.json())
-    .then((response) => {
-      response.forEach(element => {
-           checkListName= element.name
-           id=element.id;
-          $("#selectCard").append('<option value='+id+'>'+checkListName+'</option>')
-      });
-        
-   
-    })
+        .then(response => response.json())
+        .then((response) => {
+            response.forEach(element => {
+                if (element.id == checklistId) {
+                    cardId = element.idCard;
+                }
+            });
+        })
+    $('#list-group').load(document.URL + ' #list-group');
+    loadTask();
+
+
+});
+
+
+function loadCard() {
+    fetch("https://api.trello.com/1/boards/" + boardID + "/checklists/?" + auth)
+        .then(response => response.json())
+        .then((response) => {
+            response.forEach(element => {
+                checkListName = element.name
+                id = element.id;
+                $("#selectCard").append('<option value=' + id + '>' + checkListName + '</option>')
+            });
+
+
+        })
 }
 
 function loadTask() {
@@ -37,14 +73,21 @@ function loadTask() {
         .then((response) => {
             checklists = response;
             console.log(response);
-            lengthOfChecklist = checklists[1]['checkItems'].length;
+            k = 0;
+            for (l = 0; l < response.length; l++) {
+                if (response[l].id == checklistId) {
+                    k = l;
+                    console.log(true + " " + k)
+                }
+            }
 
+            lengthOfChecklist = checklists[k]['checkItems'].length;
             for (i = 0; i < lengthOfChecklist; i++) {
-                checkItemName = checklists[1]['checkItems'][i]['name']
-                checkItemId = "a" + checklists[1]['checkItems'][i]['id'];
+                checkItemName = checklists[k]['checkItems'][i]['name']
+                checkItemId = "a" + checklists[k]['checkItems'][i]['id'];
                 //  console.log(checkItemId);
                 var statusOfTask;
-                if (checklists[1]['checkItems'][i]['state'] == 'incomplete') {
+                if (checklists[k]['checkItems'][i]['state'] == 'incomplete') {
 
                     statusOfTask = "unchecked";
                 } else {
@@ -58,7 +101,7 @@ function loadTask() {
 };
 
 function appendTask(statusOfTask, checkItemName, checkItemId) {
-    $(".list-group").append('<div id="parent"><div> <input type="checkbox" onclick= "handleClick(event)" id=' + checkItemId + " " + statusOfTask + ' ></input><label>' + checkItemName + '</label></div><input class="delete-button" type="button" value="delete" onclick="deleteTask(event)"></div>');
+    $("#list-group").append('<div id="parent"><div> <input type="checkbox" onclick= "handleClick(event)" id=' + checkItemId + " " + statusOfTask + ' ></input><label>' + checkItemName + '</label></div><input class="delete-button" type="button" value="delete" onclick="deleteTask(event)"></div>');
     //console.log($(.psrseInt( checkItemId)).id)
 }
 
@@ -72,14 +115,12 @@ $("#add-task").click(function () {
         fetch('https://api.trello.com/1/checklists/' + checklistId + '/checkItems?name=' + task + '&pos=bottom&' + auth, {
                 method: "POST"
             })
-            .then(response => response.json()) 
+            .then(response => response.json())
             .then((response) => {
                 console.log(response);
             })
 
-
         location.reload(true);
-
 
 
     }
@@ -88,16 +129,18 @@ $("#add-task").click(function () {
 
 function handleClick(e) {
     taskId = e.currentTarget.id.substr(1);
-//    console.log(taskId);
-console.log(cardId)
-console.log(checklistId)
-console.log(taskId)
+    //    console.log(taskId);
+    //console.log(cardId)
+    //console.log(checklistId)
+    //console.log(taskId)
     if (e.currentTarget.checked == false) {
-        fetch('https://api.trello.com/1/cards/'+cardId+'/checklist/'+checklistId+'/checkItem/' + taskId + '/state?' + auth + '&value=false', {
-            method: "PUT"})
+        fetch('https://api.trello.com/1/cards/' + cardId + '/checklist/' + checklistId + '/checkItem/' + taskId + '/state?' + auth + '&value=false', {
+            method: "PUT"
+        })
     } else {
-        fetch('https://api.trello.com/1/cards/'+cardId+'/checklist/'+checklistId+'/checkItem/' + taskId + '/state?' + auth + '&value=true', {
-            method: "PUT"})
+        fetch('https://api.trello.com/1/cards/' + cardId + '/checklist/' + checklistId + '/checkItem/' + taskId + '/state?' + auth + '&value=true', {
+            method: "PUT"
+        })
     }
 }
 
