d497cbc87d5ce2b845bde61d090c0422e42d6831
sameer ranjan <sameer.ranjan@mountblue.io>
2018-09-20 14:13:48 +0530

    global variable checkListId and cardId is removed

--- js/todo.js
+++ js/todo.js
@@ -2,9 +2,6 @@ const key = '2741f097a35c836f5878ad3536c6bcf3';
 const token = '676e08999f4ddfb4bf19968bfe184927a508a6308b7f9fc84313e85fbf7e019e';
 const auth = `key=${key}&token=${token}`;
 let boardID;
-let checklistId;
-let cardId;
-
 
 $(document).ready(() => {
   fetch(
@@ -12,37 +9,18 @@ $(document).ready(() => {
   ).then(boards => boards.json())
     .then((boards) => {
       boardID = boards[3].id;
-    }).then(() => {
-      fetch(`https://api.trello.com/1/boards/${boardID}/checklists/?${auth}`)
-        .then(checkLists => checkLists.json())
-        .then((checkLists) => {
-          checklistId = checkLists[0].id;
-          cardId = checkLists[0].idCard;
-        });
-
-      loadCard();// eslint-disable-line no-use-before-define
+      loadCheckLists();// eslint-disable-line no-use-before-define
       loadTask(); // eslint-disable-line no-use-before-define
     });
 });
 
 $('select#selectCard').change(() => {
-  checklistId = $('#selectCard option:selected').val();
-
-  fetch(`https://api.trello.com/1/boards/${boardID}/checklists/?${auth}`)
-    .then(response => response.json())
-    .then((response) => {
-      response.forEach((element) => {
-        if (element.id === checklistId) {
-          cardId = element.idCard;
-        }
-      });
-    });
   $('#list-group').load(`${document.URL} #list-group`);
   loadTask();// eslint-disable-line no-use-before-define
 });
 
 
-function loadCard() {
+function loadCheckLists() {
   fetch(`https://api.trello.com/1/boards/${boardID}/checklists/?${auth}`)
     .then(list => list.json())
     .then((list) => {
@@ -60,8 +38,7 @@ function loadTask() {
     .then((list) => {
       checklists = list;
       list.forEach((checkList) => {
-        if (checkList.id === checklistId) {
-          //    console.log(checkList)
+        if (checkList.id === $('#selectCard option:selected').val()) {
           checkList.checkItems.forEach((checkItem) => {
             checkItemName = checkItem.name;
             checkItemId = `a${checkItem.id}`;
@@ -84,6 +61,7 @@ function appendTask(statusOfTask, checkItemName, checkItemId) {
 
 
 $('#add-task').click(() => {
+  checklistId = $('#selectCard option:selected').val();
   const task = $('.todoText').val();
   if (task !== '') {
     fetch(`https://api.trello.com/1/checklists/${checklistId}/checkItems?name=${task}&pos=bottom&${auth}`, {
@@ -104,7 +82,15 @@ function checkTask(event) { // eslint-disable-line no-unused-vars
   } else {
     stateOfTask = true;
   }
-  fetch(`https://api.trello.com/1/cards/${cardId}/checklist/${checklistId}/checkItem/${taskId}/state?${auth}&value=${stateOfTask}`, {
-    method: 'PUT',
-  });
+  let cardId;
+  checklistId = $('#selectCard option:selected').val();
+  fetch(`https://api.trello.com/1/checklists/${checklistId}/cards?${auth}`)
+    .then(checkListDetail => checkListDetail.json())
+    .then((checkListDetail) => {
+      cardId = checkListDetail[0].id;
+    }).then(() => {
+      fetch(`https://api.trello.com/1/cards/${cardId}/checklist/${checklistId}/checkItem/${taskId}/state?${auth}&value=${stateOfTask}`, {
+        method: 'PUT',
+      });
+    });
 }
